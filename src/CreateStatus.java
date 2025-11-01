fe8614b58ceb504aa82949083e0167c948a4b97b
SISU <atgsisu@gmail.com>
2025-11-01 09:54:07 +0900

    chore: 'Since' 오타 수정

--- src/CreateStatus.java
+++ src/CreateStatus.java
@@ -30,7 +30,7 @@ public class CreateStatus {
 	}
 	//요청이 GET일 때 반환하는 상태 코드
 	public static int returnStatusGet(HttpRequest request) {
-		if(request.getHeaders().containsKey("if-Modifided-Scince"))
+		if(request.getHeaders().containsKey("if-Modifided-Since"))
 			return 304;
 		else {
 			String uri = request.getUri();
